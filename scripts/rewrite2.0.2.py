402d7cab1d4ba5bb8f5da911d3d1eea0929bb787
Arun Sivakumar <arun.s@circles.asia>
2017-12-21 19:07:03 +0800

    change script to to hold api endpoints

--- scripts/rewrite2.0.2.py
+++ scripts/rewrite2.0.2.py
@@ -31,7 +31,25 @@ def request(flow):
         print("\nResponse : " + str(flow.response.content)+"\n")
 
 def response(flow: http.HTTPFlow) -> None:
-    rewrite = False
+
+    dashboard = (True,("/api/2/account/batch/","mock/dashboard_batch.json"))
+    customize = (True,("/api/2/account/batch/0dcf9ee54dddd0cd2cc0b947a16c6044/","mock/account_batch.json"))
+
+    apiEndPointList = [dashboard,customize]
+
+    for endpoint in apiEndPointList:
+        if flow.request.oldpath.startswith(endpoint[1][0]) and (endpoint[0] == True):
+            mockjson = open(endpoint[1][1], "rb")
+            flow.response.content = mockjson.read()
+            flow.response.reason = "OK";
+            flow.response.status_code = 200;
+            print("\nResponse Type: " + str(flow.response.content)+"\n")
+            break
+
+
+
+ '''           
+    #rewrite = False
 
     # if flow.request.oldpath.endswith("/revision?"):
     # 	mockjson = open("mock/revision.json", "r")
@@ -42,14 +60,17 @@ def response(flow: http.HTTPFlow) -> None:
     # if flow.request.oldpath.startswith("/api/2/account/my/profile/details/get/"):
     # 	mockjson = open("mock/my_profile_details_get.json", "r")
     #     rewrite = True
-    if flow.request.oldpath.startswith("/api/2/account/batch/"):
-        mockjson = open("mock/account_batch.json", "rb")
-        rewrite = True
+
+    
+    #if flow.request.oldpath.startswith("/api/2/account/batch/"):
+        #mockjson = open("mock/dashboard_batch.json", "rb")
+        #rewrite = True
         #print("\nResponse : " + str(mockjson.read())+"\n")
 
-    if rewrite:
-        time.sleep(0.3)            
-        flow.response.content = mockjson.read()
-        flow.response.reason = "OK";
-        flow.response.status_code = 200;
-        print("\nResponse Tuype: " + str(flow.response.content)+"\n")
+def rewrite(flow: http.HTTPFlow,mockjson: bytes):
+    time.sleep(0.3)            
+    flow.response.content = mockjson.read()
+    flow.response.reason = "OK";
+    flow.response.status_code = 200;
+    print("\nResponse Tuype: " + str(flow.response.content)+"\n")
+    ```
